6d008c34dadba00f8c551c5e35d8839f49b2a1b3
irina08 <irina_ger@yahoo.com>
2020-08-20 22:06:52 -0700

    Added some tests

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -13,5 +13,6 @@ describe('My First Test Suite', () => {
     //second way through chaining
     cy.get('.products').find('.product').should('have.length',4);
 
+    cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
   })
 })
