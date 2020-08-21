44c2fad4981a8a72f573e1508025b74a093f6739
irina08 <irina_ger@yahoo.com>
2020-08-20 21:58:22 -0700

    Added some tests

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -6,7 +6,12 @@ describe('My First Test Suite', () => {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
     cy.get('.search-keyword').type('ca');
     cy.wait(2000);
+
+    cy.get('.product').should('have.length', 5);
     cy.get('.product:visible').should('have.length',4);
+
+    //second way through chaining
+    cy.get('.products').find('.product').should('have.length',4);
 
   })
 })
