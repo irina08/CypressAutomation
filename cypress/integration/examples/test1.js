d6324f176a82e263555c72558a2daf0ff85a9c92
irina08 <irina_ger@yahoo.com>
2020-08-20 22:08:52 -0700

    Added comments to tests

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -13,6 +13,7 @@ describe('My First Test Suite', () => {
     //second way through chaining
     cy.get('.products').find('.product').should('have.length',4);
 
+    //element with second index ad to cart
     cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
   })
 })
