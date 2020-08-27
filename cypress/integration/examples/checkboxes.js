00951b0f6a2a73c32b891e30e80b33685e383a0d
irina08 <irina_ger@yahoo.com>
2020-08-27 10:08:52 -0700

    added tests to checkboxes.js

--- cypress/integration/examples/checkboxes.js
+++ cypress/integration/examples/checkboxes.js
@@ -6,6 +6,9 @@ describe('CheckBox Test Suite', () => {
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
     cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1');
 
+  //uncheck checkbox
+    cy.get('#checkBoxOption1').uncheck().should('not.be.checked');
+
 
 
   })
