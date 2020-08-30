230d305f4699ef9644c5030b4096b5630aeb0dc3
irina08 <irina_ger@yahoo.com>
2020-08-29 17:39:51 -0700

    added visibleInvisible.js

---
+++ cypress/integration/examples/visibleInvisible.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress" />
+
+describe('Visible Invisible elements Test Suite', () => {
+  it('My First Test case', () => {
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+
+
+  })
+
+})
