3ed19901e9168bef0da3948b2a686ccc2c0ab170
irina08 <irina_ger@yahoo.com>
2020-08-30 00:03:12 -0700

    added some tests to visibleInvisible.js

--- cypress/integration/examples/visibleInvisible.js
+++ cypress/integration/examples/visibleInvisible.js
@@ -4,7 +4,12 @@ describe('Visible Invisible elements Test Suite', () => {
   it('My First Test case', () => {
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
 
+    cy.get('#displayed-text').should('be.visible');
+    cy.get('#hide-textbox').click();
+    cy.get('#displayed-text').should('not.be.visible');
+    cy.get('#show-textbox').click();
+    cy.get('#displayed-text').should('be.visible');
 
-  })
+  });
 
-})
+});
