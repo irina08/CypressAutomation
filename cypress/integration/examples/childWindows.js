12395f9ed66c505f240036d4bac4726c9939158f
irina08 <irina_ger@yahoo.com>
2020-08-31 00:33:08 -0700

    added tests to childWindows.js

--- cypress/integration/examples/childWindows.js
+++ cypress/integration/examples/childWindows.js
@@ -4,8 +4,11 @@ describe('Child Windows Test Suite', () => {
   it('My First Test case', () => {
     cy.visit("http://rahulshettyacademy.com/AutomationPractice/");
 
+    //prop - non cypress command like text(method we need to resolve promise)
+    cy.get('#opentab').then(function(el) {
+      const url = el.prop('href');
 
-
+    });
 
   });
 
