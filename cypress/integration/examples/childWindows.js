969c414e5733c5c6b3561d1aeff3d40f3781ec8f
irina08 <irina_ger@yahoo.com>
2020-08-31 00:32:27 -0700

    added childWindows.js

---
+++ cypress/integration/examples/childWindows.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+describe('Child Windows Test Suite', () => {
+  it('My First Test case', () => {
+    cy.visit("http://rahulshettyacademy.com/AutomationPractice/");
+
+
+
+
+  });
+
+});
