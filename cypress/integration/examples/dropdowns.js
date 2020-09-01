8f2c9fa311019f8ff794bd8bf7158d2712a8015e
irina08 <irina_ger@yahoo.com>
2020-09-01 00:06:36 -0700

    handle static dropdowns

--- cypress/integration/examples/dropdowns.js
+++ cypress/integration/examples/dropdowns.js
@@ -4,6 +4,8 @@ describe('Dropdown Test Suite', () => {
   it('My SecondTest case', () => {
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
 
+    //static dropdown
+    cy.get('select').select('option2').should('have.value', 'option2');
 
 
   });
