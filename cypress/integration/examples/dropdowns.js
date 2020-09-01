e041a223035fc9bc6e81b4d83a922b37170207fe
irina08 <irina_ger@yahoo.com>
2020-09-01 00:06:01 -0700

    added dropdowns.js

---
+++ cypress/integration/examples/dropdowns.js
@@ -0,0 +1,11 @@
+/// <reference types="Cypress" />
+
+describe('Dropdown Test Suite', () => {
+  it('My SecondTest case', () => {
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+
+
+
+  });
+
+});
