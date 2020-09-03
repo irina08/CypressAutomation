33796f43a102e278f6ca77df95d5a43f94acbf94
irina08 <irina_ger@yahoo.com>
2020-09-03 00:24:22 -0700

    how to switch to frames

--- cypress/integration/examples/frames.js
+++ cypress/integration/examples/frames.js
@@ -7,6 +7,10 @@ describe('Frames Test Suite', () => {
     cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
     cy.frameLoaded('#courses-iframe');
 
+    //switch to iframe
+    cy.iframe().find('a[href*="mentorship"]').eq(0).click();
+    cy.iframe().find('h1[class*="pricing-title"]').should('have.length', 2);
+
 
   });
 
