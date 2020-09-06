78890af8d8e96c201d5dd19ad87343d0f956b5f8
irina08 <irina_ger@yahoo.com>
2020-09-05 22:38:13 -0700

    added navigation back to popups.js

--- cypress/integration/examples/popups.js
+++ cypress/integration/examples/popups.js
@@ -17,12 +17,13 @@ describe('Popups Test Suite', () => {
       expect(str).to.equal('Hello , Are you sure you want to confirm?');
     });
 
-  //removeAttr in order to open page in the same window
+    //removeAttr in order to open page in the same window
     cy.get('#opentab').invoke('removeAttr', 'target').click();
 
     cy.url().should('include', 'rahulshettyacademy');
 
-
+    //navigation back
+    cy.go('back');
 
   });
 
