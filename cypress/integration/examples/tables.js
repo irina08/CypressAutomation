7ce56910bc941fa8fa06458562fc3148b478ecb2
irina08 <irina_ger@yahoo.com>
2020-09-08 08:09:39 -0700

    added tables.js

---
+++ cypress/integration/examples/tables.js
@@ -0,0 +1,23 @@
+/// <reference types="Cypress" />
+
+describe('Tables Test Suite', () => {
+  it('My First Test case', () => {
+    cy.visit("http://rahulshettyacademy.com/AutomationPractice/");
+
+    cy.get('tr td:nth-child(2)').each(($el, index, $list) => {
+      const text = $el.text();
+      if(text.includes('Python')) {
+
+        //how to get next colon text in table, for example text in second row,
+        // we need to find price in third row
+        cy.get('tr td:nth-child(2)').eq(index).next().then(function(price) {
+          const priceText = price.text();
+          expect(priceText).to.equal('25');
+        });
+      }
+
+    });
+
+  });
+
+});
