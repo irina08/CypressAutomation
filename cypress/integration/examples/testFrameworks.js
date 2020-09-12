b8e691f8e9cc5e503bb041e678ef57e86321b354
irina08 <irina_ger@yahoo.com>
2020-09-12 12:12:51 -0700

    added some tests to testFrameworks.js

--- cypress/integration/examples/testFrameworks.js
+++ cypress/integration/examples/testFrameworks.js
@@ -17,6 +17,12 @@ describe('Block Test Suite', () => {
     //cy.get('select').select('Female');
     cy.get('select').select(this.data.gender);
 
+    cy.get(':nth-child(4) > .ng-untouched').should('have.value', this.data.name);
+    cy.get('input[name="name"]:nth-child(2)').should('have.attr', 'minlength', '2');
+    cy.get('#inlineRadio3').should('be.disabled');
+    cy.get(':nth-child(2) > .nav-link').click();
+
+
 
   });
 
