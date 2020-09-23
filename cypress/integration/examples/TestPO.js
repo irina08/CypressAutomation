5b9b3845cc805701aa1360d0b86125ba94cb6ccc
irina08 <irina_ger@yahoo.com>
2020-09-23 00:12:09 -0700

    added tests to TestPO.js

--- cypress/integration/examples/TestPO.js
+++ cypress/integration/examples/TestPO.js
@@ -22,6 +22,9 @@ describe('Page Object Test Suite', () => {
     homePage.getEditBox().type(this.data.name);
     homePage.getGender().select(this.data.gender);
     homePage.getTwoWayDataBinding().should('have.value', this.data.name);
+    homePage.getEditBox().should('have.attr', 'minlength', '2');
+    homePage.getEntrepreneaur().should('be.disabled');
+    homePage.getShopTab().click();
 
 
   });
