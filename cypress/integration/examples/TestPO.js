42e1a4ccc2aeedb27b059d055ae14a29c6a25ac7
irina08 <irina_ger@yahoo.com>
2020-09-28 23:16:02 -0700

    added some tests to TestPO.js

--- cypress/integration/examples/TestPO.js
+++ cypress/integration/examples/TestPO.js
@@ -56,8 +56,10 @@ describe('Page Object Test Suite', () => {
     cy.get('input[type="submit"]').click();
 
     //cy.get('.alert').should('have.text', 'Success! Thank you! Your order will be delivered in next few weeks :-)');
-
-
+    cy.get('.alert').then((element) => {
+      const actualText = element.text();
+      expect(actualText.includes("Success")).to.be.true;
+    })
   });
 
 });
