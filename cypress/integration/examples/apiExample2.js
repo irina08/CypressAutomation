09f0b6b9a1fd86fcd595ee81b6c82c08c06223b0
irina08 <irina_ger@yahoo.com>
2020-12-14 00:43:48 -0800

    added api test to apiExample2.js

---
+++ cypress/integration/examples/apiExample2.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress" />
+
+
+describe('API test', () => {
+  it('My Second API Test case', () => {
+
+    cy.request('POST', 'http://216.10.245.166/Library/Addbook.php', {
+      "name": "Learn Appium Automation with Java",
+      "isbn": "bcdsss",
+      "aisle": "22s7",
+      "author": "John foe"
+    }).then((response) => {
+      expect(response.body).to.have.property("Msg", "successfully added");
+      expect(response.status).to.eq(200);
+    });
+
+  });
+});
